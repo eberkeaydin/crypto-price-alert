559aaaf87ba4e183f3a3ede8e782c086bffcc94b
metinkeremurkmez <metinkeremurkmez@gmail.com>
2025-02-11 01:50:49 +0300

    start sh to gke

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 npm install -g pm2
-pm2 start lib/Cronjobs.ts --name cronjobs
-pm2 start lib/KafkaConsumer.ts --name kafka-consumer
-pm2 start index.js --name app
+pm2 start lib/Cronjobs.ts --name cronjobs --interpreter node
+pm2 start lib/KafkaConsumer.ts --name kafka-consumer --interpreter node
+pm2 start index.js --name app --interpreter node
 pm2 logs --raw | tee -a /app/pm2_logs.txt
