2ea93d438c628d2b098ee687b9a07908bf46a9ee
metinkeremurkmez <metinkeremurkmez@gmail.com>
2025-02-11 02:21:34 +0300

    start sh to gke

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 npm install -g pm2
 npm install -g ts-node
-pm2 start lib/Cronjobs.ts --name cronjobs --interpreter ts-node
-pm2 start lib/KafkaConsumer.ts --name kafka-consumer --interpreter ts-node
-pm2 start index.js --name app --interpreter node
+
+pm2 start lib/Cronjobs.ts --name cronjobs --interpreter node --node-args="--loader ts-node/esm"
+pm2 start lib/KafkaConsumer.ts --name kafka-consumer --interpreter node --node-args="--loader ts-node/esm"
+pm2 start index.js --name app
 pm2 logs --raw | tee -a /app/pm2_logs.txt
